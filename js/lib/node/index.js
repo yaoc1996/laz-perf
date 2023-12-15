060e84450428a1d7f441d212c4e4b5dfdc017f93
yaoc1996 <yaoc1996@gmail.com>
2023-12-15 13:02:11 -0500

    added lib

---
+++ js/lib/node/index.js
@@ -0,0 +1,11 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.LazPerf = exports.create = exports.createLazPerf = void 0;
+/* eslint-disable-next-line */
+const laz_perf_js_1 = __importDefault(require("./laz-perf.js"));
+exports.createLazPerf = laz_perf_js_1.default;
+exports.create = laz_perf_js_1.default;
+exports.LazPerf = { create: laz_perf_js_1.default };
